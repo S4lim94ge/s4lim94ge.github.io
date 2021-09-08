a8041e05432d5b3474124b009c82d3c033de1162
S4lim94ge <89635041+S4lim94ge@users.noreply.github.com>
2021-09-08 08:55:03 -0700

    sa

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> THE7ERROR Blog </title>
+</head>
+<style type="text/css">
+    body {
+         background: black;
+         background-color: black;
+         color: black;
+         text-align: center;
+    }
+    .t7e {
+         font-family: monospace;
+         color: white;
+    }
+    .tengah {
+       position: absolute;
+     margin: auto;
+     height: 50%;
+     top: 0;
+     bottom: 0;
+     left: 0;
+     right: 0;
+    }
+</style>
+<center>
+<div class="tengah">
+<br><br><br><br><br>
+<h1 class="t7e" style="text-shadow: red 0px 0px 10px;"> THE7ERROR </h1>
+<h2 class="t7e"> We Working on Website right now </h3>
+<h2 class="t7e">Please Wait A few Weeks</h2>
+</div>
+</center>
+</html>
